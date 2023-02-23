c694447f7b519e2af8f10179d00c41f5caf14a25
Tina.Hpr <elharper1234@gmail.com>
2023-02-23 23:17:29 +0100

    pushing startable bot

--- DSharpBot/DSharpBot/Program.cs
+++ DSharpBot/DSharpBot/Program.cs
@@ -1,17 +1,27 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 public class Program
 {
     public static Task Main(string[] args) => new Program().MainAsync();
+    
     private DiscordSocketClient _client;
+    private IConfigurationRoot _config;
     public async Task MainAsync()
     {
+
+        _config = new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("config.json")
+        .Build();
+
         _client = new DiscordSocketClient();
         _client.Log += Log;
-        await _client.LoginAsync(TokenType.Bot, "token");
+        await _client.LoginAsync(TokenType.Bot, _config["BOT_TOKEN"]);
         await _client.StartAsync();
-        
+
+        await Task.Delay(-1);
     }
     private Task Log(LogMessage msg)
     {
