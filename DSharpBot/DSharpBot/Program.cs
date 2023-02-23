1456ad099ae9a79fa6485ec94db14b70ea5d9ede
Tina.Hpr <elharper1234@gmail.com>
2023-02-23 22:57:20 +0100

    initial push

--- DSharpBot/DSharpBot/Program.cs
+++ DSharpBot/DSharpBot/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+    private DiscordSocketClient _client;
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        await _client.LoginAsync(TokenType.Bot, "token");
+        await _client.StartAsync();
+        
+    }
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
