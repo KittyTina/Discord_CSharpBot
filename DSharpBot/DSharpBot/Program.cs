c62429d385c9206bef5bf706d586211c28e8625b
Tina.Hpr <elharper1234@gmail.com>
2023-02-25 00:47:04 +0100

    Fixed that program would throw an exception when trying to retrieve data but fails with it.

--- DSharpBot/DSharpBot/Program.cs
+++ DSharpBot/DSharpBot/Program.cs
@@ -52,7 +52,14 @@ public class Program
                     return;
                 }
                 Embed stockEmbed = await GetStockDataAsync(symbol);
-                await command.RespondAsync(embeds: new[] { stockEmbed });
+                if (stockEmbed != null)
+                {
+                    await command.RespondAsync(embeds: new[] { stockEmbed });
+                }
+                else
+                {
+                    await command.RespondAsync("Error: Unable to retrieve stock data.");
+                }
                 break;
         }
     }
@@ -96,13 +103,30 @@ public class Program
                 string responseContent = await responseMessage.Content.ReadAsStringAsync();
                 JObject responseData = JObject.Parse(responseContent);
                 JToken timeSeriesDay = responseData["Time Series (Daily)"];
-                JProperty latestPriceProperty = timeSeriesDay.FirstOrDefault().ToObject<JObject>().Properties().FirstOrDefault(property => property.Name.EndsWith("close"));
-                if (latestPriceProperty != null)
+                //JProperty latestPriceProperty = timeSeriesDay.FirstOrDefault().ToObject<JObject>().Properties().FirstOrDefault(property => property.Name.EndsWith("close"));
+                if (timeSeriesDay is JObject timeSeriesObj)
                 {
-                    decimal latestPrice = (decimal)latestPriceProperty.Value;
-                    string title = $"Stock data for {symbol}";
-                    string description = $"Latest closing price: {latestPrice:C}";
-                    return CreateEmbed(title, description, Color.Green);
+                    // Handle JObject case
+                    JProperty latestPriceProperty = timeSeriesObj.Properties().FirstOrDefault(property => property.Name.EndsWith("close"));
+                    if (latestPriceProperty != null)
+                    {
+                        decimal latestPrice = (decimal)latestPriceProperty.Value;
+                        string title = $"Stock data for {symbol}";
+                        string description = $"Latest closing price: {latestPrice:C}";
+                        return CreateEmbed(title, description, Color.Green);
+                    }
+                }
+                else if (timeSeriesDay is JProperty timeSeriesProp && timeSeriesProp.Value is JObject timeSeriesPropObj)
+                {
+                    // Handle JProperty case
+                    JProperty latestPriceProperty = timeSeriesPropObj.Properties().FirstOrDefault(property => property.Name.EndsWith("close"));
+                    if (latestPriceProperty != null)
+                    {
+                        decimal latestPrice = (decimal)latestPriceProperty.Value;
+                        string title = $"Stock data for {symbol}";
+                        string description = $"Latest closing price: {latestPrice:C}";
+                        return CreateEmbed(title, description, Color.Green);
+                    }
                 }
             }
         }
